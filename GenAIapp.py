cc7441588b637fbf84e3be89f0e95e63bfb2bd25
praveenrajan06-cmd <praveenrajan06@gmail.com>
2026-06-26 15:05:56 +0530

    Update GenAIapp.py

--- GenAIapp.py
+++ GenAIapp.py
@@ -19,7 +19,7 @@ unsafe_allow_html=True,
 )
 robo = genai.Client( api_key=st.secrets["GOOGLE_API_KEY"])
 mychat = robo.chats.create(model="gemini-flash-lite-latest")
-Placeholder for the response
+##Placeholder for the response
 response_placeholder = st.empty()
 question = st.text_input("", placeholder="Enter your Python question here...")
 col1, col2, col3 = st.columns([4, 1, 4])
